8a2476aebc5acec238da812c4954d0d15c362de5
DiegoNM95 <diegomartinez8995@gmail.com>
2018-03-07 16:05:44 -0600

    Agragando proyecto MiembrosColegio de Java

---
+++ src/miembroscolegio/MiembrosColegio.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package miembroscolegio;
+
+/**
+ *
+ * @author ludie
+ */
+public class MiembrosColegio {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
