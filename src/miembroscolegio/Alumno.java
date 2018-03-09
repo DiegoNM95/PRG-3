201045b99835e4a327977d2843782ed90499946a
CesarS88 <sanchezkevin8888@gmail.com>
2018-03-09 17:14:02 -0600

    Se agregaron atributos y metodos a la clase Alumno

--- src/miembroscolegio/Alumno.java
+++ src/miembroscolegio/Alumno.java
@@ -5,10 +5,44 @@
  */
 package miembroscolegio;
 
+import java.util.Date;
+
 /**
  *
  * @author Cesar Kevin Sanchez Garcia
  */
 public class Alumno {
+    private Date fechaMatricula;
+    private int gradoAcademico;
+    private String nombreResponsable;
+    private int numeroResponsable;
     
+    public Alumno(Date fechaMatricula, int gradoAcademico, String nombreResponsable, int numeroResponsable){
+        this.fechaMatricula = fechaMatricula;
+        this.gradoAcademico = gradoAcademico;
+        this.nombreResponsable = nombreResponsable;
+        this.numeroResponsable = numeroResponsable;
+        
+    }
+    
+    public Date getFechaMatricula (){
+        return fechaMatricula;
+        
+    }
+    
+    public int getGradoAcademico (){
+        return gradoAcademico;
+    }
+    
+    public String getNombreResponsable (){
+        return nombreResponsable;
+    }
+    
+    public int getNumeroReponsable(){
+        return numeroResponsable;
+    }
+    
+    public String mostrarDetalles(){
+        return null;
+    }
 }
