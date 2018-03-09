5fb095bed77ac79ccf80e5f7082232e3ba9fc325
HenryG96 <henrry14_cr9cr8@hotmail.com>
2018-03-09 17:28:31 -0600

    Se agrego atributos y metodos a la clase Profesor

--- src/miembroscolegio/Profesor.java
+++ src/miembroscolegio/Profesor.java
@@ -5,10 +5,41 @@
  */
 package miembroscolegio;
 
+import java.math.BigDecimal;
+import java.util.Date;
+
 /**
  *
- * @author ACP
+ * @Henry Moises Marinero Gutierrez.
  */
 public class Profesor {
+    private BigDecimal sueldo;
+    private String dui;
+    private Date fechaIngreso;
+    
+    public Profesor(BigDecimal sueldo, String dui, Date fechaIngreso){
+        this.sueldo = sueldo;
+        this.dui = dui;
+        this.fechaIngreso= fechaIngreso;
+        
+      
+    }
+      public BigDecimal getSueldo(){
+        return sueldo;
+            
+        }
+      
+      public String getdui(){
+        return dui;
+            
+        }
+      public Date getfechaIngreso(){
+        return fechaIngreso;
+            
+        }
+      public String mostrarDetalle(){
+        return null;
+            
+        }
     
 }
