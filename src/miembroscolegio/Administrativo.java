4b51006ffeca1c5649c3cc598bb6085703a9aa5a
oscarvillacorta <ozkar.villacorta0915@outlook.com>
2018-03-11 13:46:26 -0600

    Se agregaron metodos y atributos a la clase Administrativo

--- src/miembroscolegio/Administrativo.java
+++ src/miembroscolegio/Administrativo.java
@@ -5,10 +5,41 @@
  */
 package miembroscolegio;
 
+import java.math.BigDecimal;
+
 /**
  *
  * @Oscar Edgardo Villacorta
  */
-public class Administrativo {
+public class  Administrativo extends Miembro {
+    private BigDecimal sueldo;
+    private String dui;
     
+    Administrativo(String nombre, String apellido, String carnet, 
+            String direccion, String genero, BigDecimal sueldo, String dui){
+        super(nombre, apellido, carnet, direccion,genero);
+        this.sueldo=sueldo;
+        this.dui=dui;
+    }    
+
+    public BigDecimal getSueldo() {
+        return sueldo;
+    }
+
+    public String getDui() {
+        return dui;
+    }
+    
+    @Override
+    public String toString(){
+        String texto="";
+        texto="Nombre: "+getNombres()+"\n";
+        texto+="Apellidos: "+getApellidos()+"\n";
+        texto+="Carnet: "+getCarnet()+"\n";
+        texto+="direccion: "+getDireccion()+"\n";
+        texto+="genero: "+getGenero()+"\n";
+        texto+="sueldo: "+getSueldo()+"\n";
+        texto+="dui: "+getDui()+"\n";
+        return texto;
+    }
 }
